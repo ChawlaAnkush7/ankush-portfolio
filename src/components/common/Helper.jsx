08d039d7494c14e2702e9e87c40bf5c48ba993e6
Bharti <bhartichawla4889@gmail.com>
2024-08-14 19:27:01 +0530

    change

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,7 +1,7 @@
 const projectCardData = [
   {
     id: 1,
-    projectImg: "../../assets/images/png/project-img.png",
+    projectImg: "../src/assets/images/png/project-img.png",
     projectTitle: "project name",
     projectDetail:
       "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
@@ -11,71 +11,6 @@ const projectCardData = [
     livePreview: "Live Preview",
     viewCode: "View Code",
     smallGitIcon: "../../assets/images/svg/github-small-icon.svg",
-  },
-  {
-    id: 2,
-    projectImg: "../src/assets/images/png/project-img.png",
-    projectTitle: "project name",
-    projectDetail:
-      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
-    techStack: "Tech stack :",
-    stackSkills: " HTML , JavaScript, SASS, React",
-    liveIcon: "../src/assets/images/svg/live-link-icon.svg",
-    livePreview: "Live Preview",
-    viewCode: "View Code",
-    smallGitIcon: "../src/assets/images/svg/github-small-icon.svg",
-  },
-  {
-    id: 3,
-    projectImg: "../src/assets/images/png/project-img.png",
-    projectTitle: "project name",
-    projectDetail:
-      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
-    techStack: "Tech stack :",
-    stackSkills: " HTML , JavaScript, SASS, React",
-    liveIcon: "../src/assets/images/svg/live-link-icon.svg",
-    livePreview: "Live Preview",
-    viewCode: "View Code",
-    smallGitIcon: "../src/assets/images/svg/github-small-icon.svg",
-  },
-  {
-    id: 4,
-    projectImg: "../src/assets/images/png/project-img.png",
-    projectTitle: "project name",
-    projectDetail:
-      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
-    techStack: "Tech stack :",
-    stackSkills: " HTML , JavaScript, SASS, React",
-    liveIcon: "../src/assets/images/svg/live-link-icon.svg",
-    livePreview: "Live Preview",
-    viewCode: "View Code",
-    smallGitIcon: "../src/assets/images/svg/github-small-icon.svg",
-  },
-  {
-    id: 5,
-    projectImg: "../src/assets/images/png/project-img.png",
-    projectTitle: "project name",
-    projectDetail:
-      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
-    techStack: "Tech stack :",
-    stackSkills: " HTML , JavaScript, SASS, React",
-    liveIcon: "../src/assets/images/svg/live-link-icon.svg",
-    livePreview: "Live Preview",
-    viewCode: "View Code",
-    smallGitIcon: "../src/assets/images/svg/github-small-icon.svg",
-  },
-  {
-    id: 6,
-    projectImg: "../src/assets/images/png/project-img.png",
-    projectTitle: "project name",
-    projectDetail:
-      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
-    techStack: "Tech stack :",
-    stackSkills: " HTML , JavaScript, SASS, React",
-    liveIcon: "../src/assets/images/svg/live-link-icon.svg",
-    livePreview: "Live Preview",
-    viewCode: "View Code",
-    smallGitIcon: "../src/assets/images/svg/github-small-icon.svg",
   },
 ];
 export default projectCardData;
