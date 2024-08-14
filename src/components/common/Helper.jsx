e5419fe10a9ef946206f717a5850980e70faf6e2
Bharti <bhartichawla4889@gmail.com>
2024-08-14 19:46:51 +0530

    project card images file path updated

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,19 +1,85 @@
-import projectImg from "../../assets/images/png/project-img.png";
-import liveIcon from "../../assets/images/svg/live-link-icon.svg";
-import smallGitIcon from "../../assets/images/svg/github-small-icon.svg";
+import projectImage from "../../assets/images/png/project-img.png";
+import cardLiveIcon from "../../assets/images/svg/live-link-icon.svg";
+import cardGitIcon from "../../assets/images/svg/github-small-icon.svg";
+
 const projectCardData = [
   {
     id: 1,
-    projectImg: projectImg,
+    projectImg: projectImage,
     projectTitle: "project name",
     projectDetail:
       "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
     techStack: "Tech stack :",
     stackSkills: " HTML , JavaScript, SASS, React",
-    liveIcon: liveIcon,
+    liveIcon: cardLiveIcon,
     livePreview: "Live Preview",
     viewCode: "View Code",
-    smallGitIcon: smallGitIcon,
+    smallGitIcon: cardGitIcon,
+  },
+  {
+    id: 2,
+    projectImg: projectImage,
+    projectTitle: "project name",
+    projectDetail:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    techStack: "Tech stack :",
+    stackSkills: " HTML , JavaScript, SASS, React",
+    liveIcon: cardLiveIcon,
+    livePreview: "Live Preview",
+    viewCode: "View Code",
+    smallGitIcon: cardGitIcon,
+  },
+  {
+    id: 3,
+    projectImg: projectImage,
+    projectTitle: "project name",
+    projectDetail:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    techStack: "Tech stack :",
+    stackSkills: " HTML , JavaScript, SASS, React",
+    liveIcon: cardLiveIcon,
+    livePreview: "Live Preview",
+    viewCode: "View Code",
+    smallGitIcon: cardGitIcon,
+  },
+  {
+    id: 4,
+    projectImg: projectImage,
+    projectTitle: "project name",
+    projectDetail:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    techStack: "Tech stack :",
+    stackSkills: " HTML , JavaScript, SASS, React",
+    liveIcon: cardLiveIcon,
+    livePreview: "Live Preview",
+    viewCode: "View Code",
+    smallGitIcon: cardGitIcon,
+  },
+  {
+    id: 5,
+    projectImg: projectImage,
+    projectTitle: "project name",
+    projectDetail:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    techStack: "Tech stack :",
+    stackSkills: " HTML , JavaScript, SASS, React",
+    liveIcon: cardLiveIcon,
+    livePreview: "Live Preview",
+    viewCode: "View Code",
+    smallGitIcon: cardGitIcon,
+  },
+  {
+    id: 6,
+    projectImg: projectImage,
+    projectTitle: "project name",
+    projectDetail:
+      "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
+    techStack: "Tech stack :",
+    stackSkills: " HTML , JavaScript, SASS, React",
+    liveIcon: cardLiveIcon,
+    livePreview: "Live Preview",
+    viewCode: "View Code",
+    smallGitIcon: cardGitIcon,
   },
 ];
 export default projectCardData;
