1b4c4782e88c1b2b289c7353c74d018d16880d1c
Bharti <bhartichawla4889@gmail.com>
2024-08-14 19:36:04 +0530

    img path changes

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,16 +1,19 @@
+import projectImg from "../../assets/images/png/project-img.png";
+import liveIcon from "../../assets/images/svg/live-link-icon.svg";
+import smallGitIcon from "../../assets/images/svg/github-small-icon.svg";
 const projectCardData = [
   {
     id: 1,
-    projectImg: "../src/assets/images/png/project-img.png",
+    projectImg: projectImg,
     projectTitle: "project name",
     projectDetail:
       "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
     techStack: "Tech stack :",
     stackSkills: " HTML , JavaScript, SASS, React",
-    liveIcon: "../../assets/images/svg/live-link-icon.svg",
+    liveIcon: liveIcon,
     livePreview: "Live Preview",
     viewCode: "View Code",
-    smallGitIcon: "../../assets/images/svg/github-small-icon.svg",
+    smallGitIcon: smallGitIcon,
   },
 ];
 export default projectCardData;
