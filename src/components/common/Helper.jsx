81cb178d05d1f36e72a8af2f4b02030536945c42
Bharti <bhartichawla4889@gmail.com>
2024-08-14 18:43:20 +0530

    change in path

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,7 +1,7 @@
 const projectCardData = [
   {
     id: 1,
-    projectImg: "/src/assets/images/png/project-img.png",
+    projectImg: "../src/assets/images/png/project-img.png",
     projectTitle: "project name",
     projectDetail:
       "This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content",
